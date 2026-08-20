e836ebd4da1531d13fd5f1d0e65c547cbc26359e
pepw <gurra.bergqvist@gmail.com>
2026-08-20 18:12:21 +0200

    fixed!

--- src/handlers/get_all_users.rs
+++ src/handlers/get_all_users.rs
@@ -1,17 +1,13 @@
+use crate::handlers::{handlerconstants::GET_ALL_USERS_SQL_COMMAND, models::User};
 use axum::{Json, extract::State, http::StatusCode};
 use sqlx::PgPool;
-use crate::handlers::{
-    handlerconstants::GET_ALL_USERS_SQL_COMMAND,
-    models::User,
-};
 
-pub async fn get_all_users(State(pool): State<PgPool>,) -> Result<Json<Vec<User>>, (StatusCode, String) {
+pub async fn get_all_users(
+    State(pool): State<PgPool>,
+) -> Result<Json<Vec<User>>, (StatusCode, String)> {
     let users = sqlx::query_as::<_, User>(GET_ALL_USERS_SQL_COMMAND)
         .fetch_all(&pool)
         .await
-        .map_err(|error| {(
-            StatusCode::INTERNAL_SERVER_ERROR,
-            error.to_string(),
-        )})?;
+        .map_err(|error| (StatusCode::INTERNAL_SERVER_ERROR, error.to_string()))?;
     Ok(Json(users))
 }
